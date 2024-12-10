06846a0fd5c25c7cfe7dcb0b751f9aaba026344a
Shashidhar <shashidhar.sanshi@gmail.com>
2024-12-10 20:08:55 +0530

    coming soon changes

---
+++ app/components/HeaderTop.js
@@ -0,0 +1,32 @@
+
+import React from 'react';
+import Image from 'next/image'
+
+
+
+const HeaderTop = () => {
+
+  return (
+    <div className='mx-auto divide-none px-6 bg-black text-white'>
+        <div className="row gx-0">
+            <div className='mx-auto'>
+                <div>
+                    <small className="me-3 text-light"></small>
+                    <small className="me-3 text-light"><i className="fa fa-phone-alt me-2"></i></small>
+                    <small className="text-light"><i className="fa fa-envelope-open me-2"></i></small>
+                </div>
+            </div>
+            <div className="col-lg-4 text-center text-lg-end">
+                <div className="align-items-center">
+                   
+                </div>
+            </div>
+        </div>
+        <div>
+        </div>
+    </div>
+
+  )
+}
+
+export default HeaderTop
